43a99e1491caef13db2032412371ecbe75908d4c
damidrevid <damidrevid>
2019-05-14 16:07:44 +0100

    Second commit to my program file

--- Book.java
+++ Book.java
@@ -2,9 +2,24 @@
 // (c) Dami
 
     public class Book {
-	private String euthor;
+	private String author;
 	private String edition;
 	private int year;
 	private String title;
 	private boolean isHardcover;
-//
+//Method
+    public void setAuthor(String Author) {
+	this.author = author;
+	}
+    public String getAuthor() {
+	return author;
+	}
+
+    public void setIshardcover() {
+	this.isHardcover = true;
+	}
+    public boolean getIshardcover();
+	return isHardcover;
+	}
+
+}
