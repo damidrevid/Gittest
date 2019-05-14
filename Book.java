86058aaa0cd6b2df794420cde940b024a2ccae04
damidrevid <damidrevid>
2019-05-14 15:02:20 +0100

    My first GitCommit

---
+++ Book.java
@@ -0,0 +1,10 @@
+// Jumping on the GIT wagon.
+// (c) Dami
+
+    public class Book {
+	private String euthor;
+	private String edition;
+	private int year;
+	private String title;
+	private boolean isHardcover;
+//
